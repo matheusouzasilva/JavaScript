3413a85282d01d9fb40a65cc758a77fe7b4d6c40
matheussouzadasilva190419@gmail.com <matheussouzadasilva190419@gmail.com>
2023-02-23 01:51:36 -0400

    Condições simples

---
+++ CursoJS/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var v = 30
+console.log(`você dirigiu a ${v}km/h`)
+if (v > 60) { // condição simples
+  console.log('Você ultrapassou o limite de velocidade, multado!')
+}
+console.log('Dirija sempre com segurança')
---
+++ CursoJS/Aula11/ex009.js
@@ -0,0 +1,7 @@
+var pais = 'Portugal'
+console.log(`Vivendo em ${pais}`)
+if (pais == 'brasil') {
+  console.log('Você é brasileiro')
+} else {
+  console.log('Você é estrangeiro')
+}
