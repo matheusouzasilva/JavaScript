c5818e63c0879ddd62c4bcf01c789915a50e2c67
Matheus Souza Da Silva <matheussouzadasilva190419@gmail.com>
2023-09-23 18:43:43 -0400

    Condições parte 2

---
+++ CursoJS/Aula12/ex013.js
@@ -0,0 +1,42 @@
+var agora = new Date()
+var diasem = agora.getDay()
+
+diasem = 0
+/*
+    0 - Domingo
+    1 - Segunda
+    2 - Terça
+    3 - Quarta
+    4 - Quinta
+    5 - Sexta
+    6 - Sábado
+*/
+
+console.log(diasem)
+
+switch(diasem) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('terça')
+        break
+    case 3:
+        console.log('Quinta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('[ERRO] Dia inválido')
+        break
+}
